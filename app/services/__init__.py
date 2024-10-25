afa9ba2683e64b8455bb798cb2fbaade735d09da
Gaurang105 <gujratigaurang@gmail.com>
2024-10-26 03:02:00 +0530

    removed redundant wordpress_service code since we've implemented the funcationality in the helpers.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,4 +2,3 @@ from .auth import create_access_token, verify_token
 from .chroma_service import ChromaService
 from .claude_service import ClaudeService
 from .s3_service import S3Service
-from .wordpress_service import WordPressService
